d306a3dc25c9296ad4fd4f4a6f7ba0d0fc982cc1
hammadaslam1 <hammadaslam308@gmail.com>
2024-03-22 15:48:26 +0500

    setup mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+// dotenv.config()
+
+// mongoose.connect(process.env.MONGODB_CON)
+
+mongoose.connect('mongodb://localhost:27017/solXraf', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log('connected')).catch(() => console.log('failed to connect')).finally(() => console.log('start'));
+
 
 const app = express()
 
 app.listen(3001, () => {
     console.log('server is running on the port 3001!');
-})
+}) 
